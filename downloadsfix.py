24f826c680fc4cebc6fac7596bf0d80781eaf271
advaith-22 <popular9adu@gmail.com>
2022-12-29 20:22:51 +0530

    v1.0.2

--- downloadsfix.py
+++ downloadsfix.py
@@ -1,30 +1,46 @@
 #!/usr/bin/env python3
+
+# Module Import
 import os
 import time
-files = os.listdir("/Users/ranisujith/Downloads")
-files = [f for f in files if os.path.isfile("/Users/ranisujith/Downloads"+'/'+f)]
-ext = []
+import shutil
 
+# Getting all files and folders in downloads folder
+filesandfolders = os.listdir("/Users/ranisujith/Downloads")
+
+#Sorting out only the files in the downloads folder
+files = []
+for f in filesandfolders:
+    if os.path.isfile("/Users/ranisujith/Downloads"+'/'+f):
+        files.append(f)
+        
+
+
+# Getting all the file types available in the downloads folder
+ext = []
 for i in files:
     extensions = os.path.splitext(i.replace(" ", ""))
     ext.append(extensions[1])
 
+# Making a folder for each file type inside the downloads folder
 for e in ext:
     try:
         os.mkdir("/Users/ranisujith/Downloads/" + e.replace(".", ""))
     except:
         continue
 
+# Moving files from downloads folder to their respective file type folders for easy viewing
 for i in files:
-    e = os.path.splitext(i.replace(" ", ""))[1]
-    if e in ext:
-        try:
-            os.system(f"mv /Users/ranisujith/Downloads/\"{i}\" /Users/ranisujith/Downloads/" + e.replace(".", ""))
-        except:
-            continue
+    n, e = os.path.splitext(i.replace(" ", ""))
+    src = "/Users/ranisujith/Downloads/" + i
+    dest = "/Users/ranisujith/Downloads/" + e.replace(".", "") + "/" + i
+    if i == ".DS_Store":
+        continue
+    else:
+        shutil.move(src, dest)
 
-
-print("Files Sorted ... ")
+# Final beautification commands
+os.system("clear")
+print("Files Sorted...")
 time.sleep(2)
 os.system("clear")
-
